4f926234f20ec66366f6494385b838136f793617
ashwani1kumar1 <104037655+ashwani1kumar1@users.noreply.github.com>
2025-05-30 16:49:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function createCounter() {
 
 const counter = createCounter();
 
-console.log(counter.increment()); // Output: 1
-console.log(counter.increment()); // Output: 2
-console.log(counter.getCount());  // Output: 2
+console.log(counter.increment()); 
+console.log(counter.increment()); 
+console.log(counter.getCount());  
